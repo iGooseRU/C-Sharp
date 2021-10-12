cd22bb3070c076aadd67d856f7c5397419781c87
Egor <8653265@gmail.com>
2021-10-12 14:30:05 +0300

    feat: created interface

---
+++ Shops/Manager/IShopManager.cs
@@ -0,0 +1,14 @@
+﻿using Shops.Classes;
+
+namespace Shops.Manager
+{
+    public interface IShopManager
+    {
+        public Shop AddShop(string shopName, int id, string address);
+        public Product RegisterProduct(string productName);
+        public void AddProductToShop(Shop shop, Product product, int productPrice, int productAmount);
+        public void ChangeProductPrice(Shop shop, Product product, int newPrice);
+        public void BuyProduct(Shop shopName, Product product, Customer customer, int productToBuyAmount);
+        public Shop FindShopWithCheapestPriceForLot(Product product, int productToBuyAmount);
+    }
+}
