cdf5916cff49382eb64fe6154b66881dedda25fb
Egor <307518@niuitmo.ru>
2021-10-14 17:55:42 +0300

    feat: added new ShopAttributes class

---
+++ Shops/Classes/ShopAttributes.cs
@@ -0,0 +1,14 @@
+﻿namespace Shops.Classes
+{
+    public class ShopAttributes
+    {
+        public ShopAttributes(int id, string address)
+        {
+            Id = id;
+            Address = address;
+        }
+
+        public int Id { get; }
+        public string Address { get; }
+    }
+}
