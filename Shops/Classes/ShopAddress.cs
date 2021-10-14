f06e87a4572bc9121aee8ef3c08de4e605026d0c
Egor <307518@niuitmo.ru>
2021-10-14 10:37:35 +0300

    feat: added shopAdressClass

---
+++ Shops/Classes/ShopAddress.cs
@@ -0,0 +1,12 @@
+﻿namespace Shops.Classes
+{
+    public class ShopAddress
+    {
+        public ShopAddress(string address)
+        {
+            Address = address;
+        }
+
+        public string Address { get; }
+    }
+}
