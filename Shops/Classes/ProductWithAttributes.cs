11402a65e8d7e8571e9703df4a5879af4101bb50
Egor <307518@niuitmo.ru>
2021-10-15 14:49:24 +0300

    feat: added ProductWithAttributes class

---
+++ Shops/Classes/ProductWithAttributes.cs
@@ -0,0 +1,18 @@
+﻿namespace Shops.Classes
+{
+    public class ProductWithAttributes
+    {
+        public ProductWithAttributes(Product product, ProductAttributes productAttributes)
+        {
+            ProductName = product.ProductName;
+            ProductId = product.ProductId;
+            ProductPrice = productAttributes.ProductPrice;
+            ProductAmount = productAttributes.ProductAmount;
+        }
+
+        public string ProductName { get; }
+        public int ProductId { get; }
+        public int ProductPrice { get; set; }
+        public int ProductAmount { get; set; }
+    }
+}
