586bd62441c6fc1eb88ffb8cce980e128a6fc30e
Egor <307518@niuitmo.ru>
2021-11-03 17:10:26 +0300

    feat: created interface

---
+++ IsuExtra/Services/IIsuExtraService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using IsuExtra.Classes;
+using Group = IsuExtra.Classes.Group;
+
+namespace IsuExtra.Services
+{
+    public interface IIsuExtraService
+    {
+        MegaFaculty AddMegaFaculty(string megaFacultyName, char facultyLetter);
+        ExtraCourse CreateAndAddExtraCourseToMegaFaculty(string extraCourseName);
+        AcademicFlow AddAcademicFlowToExtraCourse(string academicFlowName, ExtraCourse extraCourse);
+        Group AddGroup(string name);
+        ExtraStudent CreateAndAddStudentToGroup(string name);
+        Lesson CreateGroupLessonAndAddToSchedule(string lessonName, LessonNums lessonNumber, DayOfWeek dayOfWeek, string teacherName, uint room);
+        Lesson CreateExtraLessonAndAddToSchedule(string lessonName, LessonNums lessonNumber, DayOfWeek dayOfWeek, string teacherName, uint room);
+        void AddStudentToAcademicFlow(ExtraStudent extraStudent);
+        void RemoveStudentFromAcademicFlow(ExtraStudent extraStudent);
+        List<AcademicFlow> GetAcademicFlows();
+        List<ExtraStudent> GetListOfStudentsWithoutExtraCourse();
+    }
+}
