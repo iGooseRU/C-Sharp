88ab797461a107e31820c40a497dd2845dc53b0f
Egor <307518@niuitmo.ru>
2021-11-24 19:30:52 +0300

    feat: created main essences

---
+++ Backups/Classes/Storage.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace Backups.Classes
+{
+    public class Storage
+    {
+        public Storage(string path)
+        {
+            FilePath = path;
+        }
+
+        public string FilePath { get; set; }
+    }
+}
