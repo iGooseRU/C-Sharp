6d8dba9ca03ac44b1caf8b0d7731133ef55a9bd7
Egor <307518@niuitmo.ru>
2021-11-26 15:52:34 +0300

    feat: added some new logic

--- Backups/Repository/VirtualRepository.cs
+++ Backups/Repository/VirtualRepository.cs
@@ -11,38 +11,12 @@ namespace Backups.Repository
             restorePoint.Storages.Add(storage);
         }
 
-        public void SingleStorageArchive(RestorePoint restorePoint)
+        public void SaveData(JobObject obj, ZipArchive zipArchive, string newFilePath)
         {
-            string archivePath = restorePoint.RestorePointPath + $@"\{restorePoint.RestorePointName}.zip";
-            foreach (JobObject obj in restorePoint.JobObjects)
-            {
-                using (ZipArchive zipArchive = ZipFile.Open(archivePath, ZipArchiveMode.Update))
-                {
-                    string pathToFileToAdd = obj.FilePath;
-                    string nameOfFileToAdd = obj.FileName;
+            string pathToFileToAdd = obj.FilePath;
+            string nameOfFileToAdd = obj.FileName;
 
-                    zipArchive.CreateEntryFromFile(pathToFileToAdd, nameOfFileToAdd);
-                }
-
-                MakeStorage(restorePoint, archivePath);
-            }
-        }
-
-        public void SplitStorageArchive(RestorePoint restorePoint)
-        {
-            foreach (JobObject obj in restorePoint.JobObjects)
-            {
-                string archivePath = restorePoint.RestorePointPath + $@"\{obj.FileName}.zip";
-                using (ZipArchive zipArchive = ZipFile.Open(archivePath, ZipArchiveMode.Create))
-                {
-                    string pathToFileToAdd = obj.FilePath;
-                    string nameOfFileToAdd = obj.FileName;
-
-                    zipArchive.CreateEntryFromFile(pathToFileToAdd, nameOfFileToAdd);
-                }
-
-                MakeStorage(restorePoint, archivePath);
-            }
+            zipArchive.CreateEntryFromFile(pathToFileToAdd, nameOfFileToAdd);
         }
     }
 }
