5c1b1925c33794515cf5b968f3dbded8b1e1e2f3
Egor <307518@niuitmo.ru>
2021-11-26 15:54:39 +0300

    feat: added some logic

--- Backups/Repository/FileSystemRepository.cs
+++ Backups/Repository/FileSystemRepository.cs
@@ -3,7 +3,7 @@ using Backups.Classes;
 
 namespace Backups.Repository
 {
-    public class FileSystemRepository
+    public class FileSystemRepository : IRepository
     {
         public void MakeStorage(RestorePoint restorePoint, string archivePath)
         {
@@ -11,41 +11,12 @@ namespace Backups.Repository
             restorePoint.Storages.Add(storage);
         }
 
-        public void SingleStorageArchive(RestorePoint restorePoint, string newObjPath)
+        public void SaveData(JobObject obj, ZipArchive zipArchive, string newFilePath)
         {
-            string archivePath = restorePoint.RestorePointPath + $@"\{restorePoint.RestorePointName}.zip";
-            foreach (JobObject obj in restorePoint.JobObjects)
-            {
-                obj.FilePath = newObjPath;
-                using (ZipArchive zipArchive = ZipFile.Open(archivePath, ZipArchiveMode.Update))
-                {
-                    string pathToFileToAdd = obj.FilePath;
-                    string nameOfFileToAdd = obj.FileName;
+            string pathToFileToAdd = obj.FilePath;
+            string nameOfFileToAdd = obj.FileName;
 
-                    zipArchive.CreateEntryFromFile(pathToFileToAdd, nameOfFileToAdd);
-                }
-
-                MakeStorage(restorePoint, archivePath);
-            }
-        }
-
-        public void SplitStorageArchive(RestorePoint restorePoint, string newObjPath)
-        {
-            foreach (JobObject obj in restorePoint.JobObjects)
-            {
-                string archivePath = restorePoint.RestorePointPath + $@"\{obj.FileName}.zip";
-                obj.FilePath = newObjPath;
-
-                using (ZipArchive zipArchive = ZipFile.Open(archivePath, ZipArchiveMode.Create))
-                {
-                    string pathToFileToAdd = obj.FilePath;
-                    string nameOfFileToAdd = obj.FileName;
-
-                    zipArchive.CreateEntryFromFile(pathToFileToAdd, nameOfFileToAdd);
-                }
-
-                MakeStorage(restorePoint, archivePath);
-            }
+            zipArchive.CreateEntryFromFile(pathToFileToAdd, nameOfFileToAdd);
         }
     }
 }
