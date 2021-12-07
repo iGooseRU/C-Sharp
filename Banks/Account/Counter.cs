306e7e7eac39ab79de3d30589181b5397f0cf1d3
Egor <307518@niuitmo.ru>
2021-12-08 01:56:29 +0300

    feat: new logic using account-type prefixes

---
+++ Banks/Account/Counter.cs
@@ -0,0 +1,38 @@
+﻿using Banks.Entities;
+
+namespace Banks.Account
+{
+    public class Counter
+    {
+        private const string CreditPrefix = "CRE";
+        private const string DepositPrefix = "DEP";
+        private const string DebitPrefix = "DEB";
+
+        private static int creditId = 100000;
+        private static int depositId = 100000;
+        private static int debitId = 100000;
+
+        public string GetId(AccountTypeFlag accountType)
+        {
+            switch (accountType)
+            {
+                case AccountTypeFlag.Credit:
+                    ++creditId;
+                    string creditRes = CreditPrefix + creditId.ToString();
+                    return creditRes;
+
+                case AccountTypeFlag.Deposit:
+                    ++depositId;
+                    string depositRes = DepositPrefix + depositId.ToString();
+                    return depositRes;
+
+                case AccountTypeFlag.Debit:
+                    ++debitId;
+                    string debitRes = DebitPrefix + debitId.ToString();
+                    return debitRes;
+            }
+
+            return null;
+        }
+    }
+}
